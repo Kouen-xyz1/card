d06221bd8beffabb4389052fdf255df8c7a01fca
Yusuke Yasuo <yasuo.y@dely.jp>
2019-05-29 20:02:16 +0900

    feat: encrypted_passwordのnilを許容する

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_20_133056) do
+ActiveRecord::Schema.define(version: 2019_05_29_105714) do
 
   create_table "dramas", force: :cascade do |t|
     t.string "title_japanese"
@@ -65,7 +65,7 @@ ActiveRecord::Schema.define(version: 2019_05_20_133056) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
+    t.string "encrypted_password"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
